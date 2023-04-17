521d000b33b9f848ae6d1c2ab618c7a5bd4687e6
Vakoi <mak-uk@mail.ru>
2023-04-17 16:03:17 +0300

    Добавьте файлы проекта.

---
+++ Практика день 1 (2)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A = 6; // замените на нужное значение
+
+        int A2 = A * A;
+        Console.WriteLine("A^2 = " + A2);
+
+        int A3 = A2 * A;
+        Console.WriteLine("A^3 = " + A3);
+
+        int A5 = A3 * A2;
+        Console.WriteLine("A^5 = " + A5);
+
+        int A10 = A5 * A5;
+        Console.WriteLine("A^10 = " + A10);
+
+        int A15 = A10 * A5;
+        Console.WriteLine("A^15 = " + A15);
+    }
+}
